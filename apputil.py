74fe21c6c2ba2a236e98d2155b0a03620d8967aa
Parmindar Singh <parmindarmundra@gmail.com>
2026-09-22 19:54:57 -0400

    update 1

--- apputil.py
+++ apputil.py
@@ -29,22 +29,37 @@ def to_binary(n):
         # recursively divide n by 2 add each remainder to n
         return to_binary(n // 2) + str(n % 2)
 
+#loading in the data set by url
 url = 'https://github.com/melaniewalsh/Intro-Cultural-Analytics/raw/master/book/data/bellevue_almshouse_modified.csv'
+# loading the data set into a pandas dataframe
 df_bellevue = pd.read_csv(url)
 
+# function task_1
 def task_1():
     df = df_bellevue.copy()
     df['gender'] = df['gender'].replace('u',pd.NA)
-    return df.isna().sum().sort_values(ascending=True).index.tolist()
+    return df.isna().sum().sort_values(ascending=True).index.sort_values().tolist()
 
+# function task_2
 def task_2():
+    """Return the total number of admissions by year in the Bellevue dataset"""
+    # create a copy of the dataset
     df = df_bellevue.copy()
+    # convert the date_in column to datetime format
     df['date_in'] = pd.to_datetime(df['date_in'])
+    # create new column called year by extracting the year from date_in column
     df['year'] = df['date_in'].dt.year
+    # group df by year and count the number of admissions & reset the index
     return df.groupby('year').size().reset_index(name='total_admissions')
 
+# function task_3
 def task_3():
+    """Return the average age of patients by gender in the Bellevue dataset"""
+    # group by the gender and the age to return the mean
     return df_bellevue.groupby('gender')['age'].mean()
 
+# function task_4
 def task_4():
+    """Return the top 5 professions in the Bellevue dataset"""
+    # take value counts of the profession column and return the top 5 professions
     return df_bellevue['profession'].value_counts().head(5).index.tolist()
